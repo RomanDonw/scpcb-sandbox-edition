0b63e02ad8224b62d4abad32ef198aab5a74de23
RomanDonw <romandonw628@gmail.com>
2026-03-04 13:06:15 +1000

    added icon changer & added automatic changing icon for output executable

--- build.bat
+++ build.bat
@@ -5,10 +5,15 @@ set blitzpath=.\Blitz3D\
 set buildfolder=.\build\
 
 "%blitzpath%\bin\blitzcc" -q -o "%buildfolder%\%outfile%" Main.bb
+
 if not %ERRORLEVEL% == 0 goto on_error
     echo Patching...
     "%blitzpath%\bin\4gb_patch" "%buildfolder%\%outfile%"
     del "%buildfolder%\%outfile%.Backup"
+
+    echo Changing icon...
+    "%blitzpath%\bin\icon-changer" .\logo.ico "%buildfolder%\%outfile%"
+
     set ERRORLEVEL=0
     goto end
 
